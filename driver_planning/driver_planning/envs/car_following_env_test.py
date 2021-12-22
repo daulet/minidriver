343c8268bfb2ce02b6774b82d4ec5d65148c55dc
Daulet Zhanguzin <daulet@zhanguzin.kz>
2021-12-22 02:09:01 -0500

    add a test to verify no dependency on pygame

--- driver_planning/driver_planning/envs/car_following_env_test.py
+++ driver_planning/driver_planning/envs/car_following_env_test.py
@@ -5,7 +5,9 @@ import driver_planning
 
 from .car import Acceleration, Lateral
 
-def test_collision_if_no_reaction():
+# Assert that if actor takes no action (change in acceleration or lateral)
+# than ego hits the other car
+def test_collision():
     env = gym.make('driver_planning:car-following-v0')
     env.reset(seed = time.time())
 
@@ -14,6 +16,18 @@ def test_collision_if_no_reaction():
         _, reward, done, _ = env.step((Acceleration.NEUTRAL, Lateral.STRAIGHT))
         total += reward
         env.render(fps=240)
+    env.close()
+
+    assert reward == -1e6
+
+def test_collision_no_render():
+    env = gym.make('driver_planning:car-following-v0')
+    env.reset(seed = time.time())
+
+    total, done = 0, False
+    while not done:
+        _, reward, done, _ = env.step((Acceleration.NEUTRAL, Lateral.STRAIGHT))
+        total += reward
     env.close()
 
     assert reward == -1e6
