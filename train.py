3934d4a0ca8086cc77b8b9ccab5d7b718a54334b
Daulet Zhanguzin <daulet@zhanguzin.kz>
2021-12-26 21:28:30 -0500

    smaller architectures learn better for now

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 import gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
 
-env = gym.make("driver_planning:car-following-v0")
+env = make_vec_env("driver_planning:car-following-v0", n_envs=8)
 
-model = PPO("MultiInputPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+model = PPO(
+  "MultiInputPolicy",
+  env,
+  verbose=1,
+  policy_kwargs={"net_arch":[16, 8, 8]})
+model.learn(total_timesteps=400000)
 model.save("models/ppo_carfollowing_10000")
