224db017337a99bc210d66299adefc44e50cb23c
Daulet Zhanguzin <daulet@zhanguzin.kz>
2021-12-28 14:59:40 -0500

    increase NN size, adjust learning rate, trained better overtaker

--- train.py
+++ train.py
@@ -6,8 +6,9 @@ from stable_baselines3.common.env_util import make_vec_env
 from test import test
 
 ITERATION = 1e5
-arch = [32, 32, 32]
+arch = [64, 64, 64]
 batch_size = 256
+learning_rate = 1e-3
 seed = int(time.time())
 
 env = make_vec_env("driver_planning:car-following-v0", n_envs=16, seed=seed)
@@ -15,15 +16,16 @@ model = PPO(
   "MultiInputPolicy",
   env,
   verbose=1,
-  batch_size=batch_size,
   policy_kwargs={"net_arch":arch},
+  batch_size=batch_size,
+  learning_rate=learning_rate,
   seed=seed,
-  tensorboard_log=f"./tensorboard/carfollowing_ppo_arch{'-'.join(map(str, arch))}_batch{batch_size}/",
+  tensorboard_log=f"./tensorboard/carfollowing_ppo_arch{'-'.join(map(str, arch))}_batch{batch_size}_lr{learning_rate}/",
 )
 
 timesteps = 0
 while True:
   model.learn(total_timesteps=ITERATION, reset_num_timesteps=False)
   timesteps+=ITERATION
-  model.save(f"./checkpoints/carfollowing_ppo_arch{'-'.join(map(str, arch))}_batch{batch_size}_{timesteps}")
+  model.save(f"./checkpoints/carfollowing_ppo_arch{'-'.join(map(str, arch))}_batch{batch_size}_lr{learning_rate}_{timesteps}")
   test(model)
